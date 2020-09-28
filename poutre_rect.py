b2b7bd298eb8c89583a034690ae6f409dea8eb08
Renel Lherisson <71229739+relhe@users.noreply.github.com>
2020-09-28 07:13:23 -0400

    Initial commit

---
+++ poutre_rect.py
@@ -0,0 +1,16 @@
+# Initialisation des variables
+
+F = 10000  # en N
+E = 210  # en GPa = 10^3 N/mm^2
+L = 100  # en mm
+b = 10  # en mm
+h = 20  # en mm
+
+# Calcul de l'inertie
+
+I = 
+
+# Calcul de la déformation maximale
+
+delta_max = 
+
---
+++ poutre_section.py
@@ -0,0 +1,22 @@
+# Initialisation des variables
+F = 10000  # en N
+E = 210  # en GPa = 10^3 N/mm^2
+L = 100  # en mm
+
+# poutre rectangulaire
+b = 10  # en mm
+h = 20  # en mm
+
+# poutre carrée
+a = 15  # en mm
+
+# poutre ronde
+d = 5  # en mm
+
+# poutre creuse
+D = 15  # en mm
+d = 5  # en mm
+
+
+# Calcul de la section optimale
+
